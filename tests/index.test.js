3841df386b91e3fb2cb3a5f593399058d941d754
IsabelGallego <isacris0919@gmail.com>
2023-03-18 20:52:05 -0500

    pruebas unitarias finalizadas

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,48 +2,23 @@ const app = require("../src/app");
 const request = require("supertest")
 const mongoose = require('mongoose');
 const Student = require('../src/models/student');
-require('dotenv').config();
 
-beforeEach(async () => {
-   jest.setTimeout(30000)
-   // Conecta a una base de datos de prueba
-   await mongoose.connect(process.env.MONGODB_URI, {
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-   });
- });
+beforeAll(async () => {
 
- afterEach(async () => {
-   // Limpia la base de datos después de cada prueba
-   await Student.deleteMany({});
-   await mongoose.connection.close();
- });
+   await mongoose.connect(process.env.MONGODB_URI, {});
+});
+ 
+afterAll(async () => {
 
-describe("GET /api/student ", () => {
+   await mongoose.disconnect();
+});
+ 
 
-   const student = new Student({ nombre: 'Simon', apellido: 'Londoño', cedula:"123456789", carrera:"Ingenieria de Sistemas" });
-
-   test("deberia retornar todos los estudiantes", async () => {
-      await student.save();
-
-      const response = await request(app).get("/api/student")
-
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toBeInstanceOf(Object)
-      expect(response.body[0].nombre).toEqual(student.nombre);
-      expect(response.body[0].apellido).toEqual(student.apellido);
-      expect(response.body[0].cedula).toEqual(student.cedula);
-      expect(response.body[0].carrera).toEqual(student.carrera);
-      expect(response.ok).toBe(true);
-
-      
-   })
-})
 
 
 describe("POST /api/student", () => {
 
-    const newStudent = { nombre: 'Simon', apellido: 'Londoño', cedula:"123456789", carrera:"Ingenieria de Sistemas" };
+   const newStudent = { nombre: 'Simon', apellido: 'Londoño', cedula: "123456789", carrera: "Ingenieria de Sistemas" };
 
    test("crear un nuevo estudiante", async () => {
       const response = await request(app).post("/api/student").send(newStudent)
@@ -59,9 +34,33 @@ describe("POST /api/student", () => {
 })
 
 
-describe ("PUT /api/student", () => {
+describe("GET /api/student ", () => {
 
-   const student = new Student({ nombre: 'Simon', apellido: 'Londoño', cedula:"123456789", carrera:"Ingenieria Industrial" });
+   const student = new Student({ nombre: 'Simon', apellido: 'Londoño', cedula: "123456789", carrera: "Ingenieria de Sistemas" });
+
+   test("deberia retornar todos los estudiantes", async () => {
+      await student.save();
+
+      const response = await request(app).get("/api/student")
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toBeInstanceOf(Object)
+      expect(response.body[0].nombre).toEqual(student.nombre);
+      expect(response.body[0].apellido).toEqual(student.apellido);
+      expect(response.body[0].cedula).toEqual(student.cedula);
+      expect(response.body[0].carrera).toEqual(student.carrera);
+      expect(response.ok).toBe(true);
+
+
+   })
+})
+
+
+
+
+describe("PUT /api/student", () => {
+
+   const student = new Student({ nombre: 'Simon', apellido: 'Londoño', cedula: "123456789", carrera: "Ingenieria Industrial" });
 
    test("Obtiene un id para modificar un estudiante", async () => {
       await student.save();
@@ -75,7 +74,7 @@ describe ("PUT /api/student", () => {
       }
 
       const response = await request(app).put(`/api/student/${id}`).send(updateStudent)
-      
+
       expect(response.statusCode).toBe(200);
       expect(response.ok).toBe(true);
       expect(response.body.acknowledged).toEqual(true);
@@ -84,9 +83,9 @@ describe ("PUT /api/student", () => {
 })
 
 
-describe ("DELETE /api/student", () => {
+describe("DELETE /api/student", () => {
 
-   const student = new Student({ nombre: 'Simon', apellido: 'Londoño', cedula:"123456789", carrera:"Ingenieria Industrial" });
+   const student = new Student({ nombre: 'Simon', apellido: 'Londoño', cedula: "123456789", carrera: "Ingenieria Industrial" });
 
    test("Borra un estudiante", async () => {
       await student.save();

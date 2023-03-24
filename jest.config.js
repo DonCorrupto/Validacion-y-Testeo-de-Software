c8c59ce1c4bee7188e9c429d106bbcc6a13b45c8
J_Alzate <georgealzate18@hotmail.com>
2023-03-24 12:42:07 -0500

    Integracion con sonarcloud

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // ...otras configuraciones...
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'src/**/*.js',
+      'src/**/*.ts',
+    ],
+    coverageReporters: ['text', 'lcov'],
+  };
+  
